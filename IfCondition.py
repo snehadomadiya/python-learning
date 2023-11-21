2328ff52f4dc19b9b3eff5a28716ab37fa01c16c
snehadomadiya <snehadomadiya1617@gmail.com>
2023-11-21 15:29:38 +0100

    implement code for compare three variable

--- IfCondition.py
+++ IfCondition.py
@@ -1,4 +1,5 @@
 #Write a code for compare Two Var which is Big
+'''
 a = 6
 b = 4
 
@@ -6,3 +7,16 @@ if a > b:
     print("a is big.")
 else:
     print("b is big.")
+''' 
+#code for compare three variable which is big
+
+a = 100
+b = 80
+c = 90
+
+if a > b and a > c:
+    print("a is big.")
+elif b > c and b > a :
+    print("b is big.")
+else:
+    print("c is big.")
