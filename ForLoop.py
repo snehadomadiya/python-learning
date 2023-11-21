b0a5cdfd593e8069c8604d328a57e4e6804ac9f5
snehadomadiya <snehadomadiya1617@gmail.com>
2023-11-21 17:24:02 +0100

    two list forloop

--- ForLoop.py
+++ ForLoop.py
@@ -1,5 +1,11 @@
-mylist = [1,2,3,4,5]
+'''mylist = [1,2,3,4,5]
 
 for s in mylist:
     print(s)
+'''
 
+students = ["sneha, chiarg, hiyan"]
+age = ["22, 21, 26"]
+
+for i, e in zip(students, age):
+    print(i, e)
