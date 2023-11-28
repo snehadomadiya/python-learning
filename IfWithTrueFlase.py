5aae23e40fe1193a984fe83ea0c21b800ef77359
snehadomadiya <snehadomadiya1617@gmail.com>
2023-11-28 14:21:34 +0100

    implement of IfWithTrueFlase

--- IfWithTrueFlase.py
+++ IfWithTrueFlase.py
@@ -6,5 +6,20 @@ print(a) """
 mylist = [1,4,8,8,5,5,4,8,6,5,8,6,2]
 a = (mylist[6] == 5)
 
-print(a)
+print(a) 
 
+if a == True:
+  print("i am happy")
+else:
+  print("i am hungry")
+
+b = (mylist[5] == 5)
+
+print(b)
+
+if b == True:
+  print("i am happy")
+else:
+  print("i am hungry")
+
+  print(b)
