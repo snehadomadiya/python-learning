030787a89b0bddde2c30d2860eeeb1a901209da5
snehadomadiya <snehadomadiya1617@gmail.com>
2023-11-28 13:47:53 +0100

    implement TrueFlase

--- TrueFlaseCondition.py
+++ TrueFlaseCondition.py
@@ -1,4 +1,4 @@
-a = True
+""" a = True
 b = False
 c = True
 
@@ -6,4 +6,14 @@ print(a or b)
 
 print(not a and b)
 
-print(not b and a)
+print(not b and a) """
+
+a = (5 > 3)
+print(a)
+
+b = (8 > 9)
+print(b)
+
+print(a or b)
+
+print(a and b)
