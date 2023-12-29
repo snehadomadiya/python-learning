d9b87b6bacc9309db074eaf62881f40d29a157da
snehadomadiya <snehadomadiya1617@gmail.com>
2023-12-29 14:41:58 +0100

    implement of MyDictionary

--- MyDictionary.py
+++ MyDictionary.py
@@ -2,12 +2,27 @@
 
 print(mydictionary["lastname"]) """
 
-student = [
+""" student = [
     {"name": "smit", "age": 28,"course":"Data", "contact":{"number":"56565656","city":"Berlin"}},
     {"name": "sneha", "age": 18,"course":"Data"},
     {"name": "akta", "age": 25,"course":"Health"},
     {"name": "akash", "age": 19,"course":"MBA"},
 ]
 
-print(student[0]["contact"])
+print(student[0]["contact"]) """
+
+student = [
+    {"name": "smit", "age": 28,"course":"Data", "contact":{"number":"56565656","city":"Berlin"}},
+    {"name": "sneha", "age": 18,"course":"Data"},
+    {"name": "akta", "age": 25,"course":"Health", "contact":{"number":"79865525","city":"Munich"}},
+    {"name": "akash", "age": 19,"course":"MBA"},
+]
+
+print(student[2]["contact"])
+print(student[0]["age"])
+print(student[2]["name"])
+print(student[3]["course"])
+
+
+
 
