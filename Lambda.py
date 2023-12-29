4e3284ef05457077cb89228a3b3f29d782bb2225
snehadomadiya <snehadomadiya1617@gmail.com>
2023-12-29 15:27:00 +0100

    implement of lambda

---
+++ Lambda.py
@@ -0,0 +1,17 @@
+""" x = lambda a, b, c: a + b + c
+print(x(5, 6, 2)) """
+
+""" x = lambda a, b, c, d: a + b + c + d
+print(x(15, 25, 20, 11)) """
+
+""" x = lambda a, b, c, d: a * b 
+print(x(15, 25, 20, 11)) """
+
+""" x = lambda a, b, c, d: a * d
+print(x(15, 25, 20, 11)) """
+
+""" x = lambda a, b, c: a / c
+print(x(125, 289, 154)) """
+
+x = lambda a, b, c: a - c
+print(x(200, 111, 122))
