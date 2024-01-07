c94d6c0207ab14b241e3a3670048be96afdaf38e
snehadomadiya <snehadomadiya1617@gmail.com>
2024-01-07 19:16:00 +0100

    implement of input

---
+++ input.py
@@ -0,0 +1,10 @@
+""" a = int(input('enter value a : '))
+b = int(input('enter value b : '))
+
+print(a+b) """
+
+#my name is sneha and my age is 25.
+name = input('enter your name: ')
+birth_year = int(input('enter your birth year: '))
+
+print("my name is " + name + " and my age is " + str(2024-birth_year))
